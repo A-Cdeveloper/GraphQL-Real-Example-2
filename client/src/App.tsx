98ab63fa854452b8fa463e5aedf81e1c720705fe
Aleksandar Cvetković <office@e-seo.info>
2025-09-30 02:00:55 +0200

    initial setup client frontend app

---
+++ client/src/App.tsx
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <div className="bg-background h-screen w-screen">
+      <div className="container mx-auto max-w-xl bg-white p-2">
+        <h1 className="font-nunito text-3xl">Nunito Font</h1>
+        <p>lorem ipsum dolor sit amet</p>
+        <p>lorem ipsum dolor sit amet</p>
+        <p>lorem ipsum dolor sit amet</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
